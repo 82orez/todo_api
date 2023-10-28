612822e6a2bd113af36afeab54e16bdd91dfef91
82orez <82orez@naver.com>
2023-10-29 05:41:23 +0900

    https + 도메인으로로 수정/ production

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require('../config/config.json')[env];
 
 // MAMP 를 설치한 경우, mysql 의 port 넘버는 8889번으로 변경되므로 포트 부분을 추가해 줌.
-const sequelize = new Sequelize(config.database, config.username, config.password, { host: config.host, dialect: 'mysql' });
+const sequelize = new Sequelize(config.database, config.username, config.password, { host: config.host, dialect: 'mysql', port: config.port });
 
 module.exports = {
   sequelize,
